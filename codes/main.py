7827943c9554472dced5582f2b112730d4c72438
Alibek <alibek060395@gmail.com>
2021-04-14 16:23:50 +0300

    added server shell and images from asus

--- codes/main.py
+++ codes/main.py
@@ -17,6 +17,41 @@ from utils import base_utils, data_utils
 import cv2
 from imutils.video import FPS
 import imagiz
+from threading import Thread
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0, device=None):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src, device)
+        (self.grabbed, self.frame) = self.stream.read()
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
 
 
 def train(opt):
@@ -256,6 +291,57 @@ def live(opt):
     cv2.namedWindow("LR image", cv2.WINDOW_NORMAL)
     cv2.namedWindow("HR image", cv2.WINDOW_NORMAL)
     # cap = cv2.VideoCapture(0)
+    cap = WebcamVideoStream(src=0).start()
+
+    # logging
+    logger = base_utils.get_logger('base')
+    if opt['verbose']:
+        logger.info('{} Configurations {}'.format('=' * 20, '=' * 20))
+        base_utils.print_options(opt, logger)
+
+    # infer and evaluate performance for each model
+    for load_path in opt['model']['generator']['load_path_lst']:
+        # setup model index
+        model_idx = osp.splitext(osp.split(load_path)[-1])[0]
+
+        # log
+        logger.info('=' * 40)
+        logger.info('Testing model: {}'.format(model_idx))
+        logger.info('=' * 40)
+
+        # create model
+        opt['model']['generator']['load_path'] = load_path
+        model = define_model(opt)
+        fps = FPS().start()
+        while True:
+            image = cap.read()
+            if image.any():
+                # print(image.shape)
+                image = cv2.resize(image, (128, 128))
+                cv2.imshow("LR image", image)
+                cv2.imwrite("LR_image.png", image)
+                norm_image = cv2.normalize(
+                    image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+                tmp_torch = torch.from_numpy(norm_image[None, :, :, :]).cuda()
+                hr_image = model.infer_live(tmp_torch)
+                cv2.imshow("HR image", hr_image[0])
+                cv2.imwrite("HR _image.png", hr_image[0])
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    cap.stop()
+                    break
+                fps.update()
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # logging
+    logger.info('Finish testing')
+    logger.info('=' * 40)
+
+
+def server(opt):
+    cv2.namedWindow("LR image", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("HR image", cv2.WINDOW_NORMAL)
     server = imagiz.TCP_Server(port=5550)
     server.start()
 
@@ -280,7 +366,6 @@ def live(opt):
         model = define_model(opt)
         fps = FPS().start()
         while True:
-            # _, frame = cap.read()
             message = server.receive()
             image = message.image
             if image.any():
@@ -310,7 +395,7 @@ if __name__ == '__main__':
     parser.add_argument('--exp_dir', type=str, required=True,
                         help='directory of the current experiment')
     parser.add_argument('--mode', type=str, required=True,
-                        help='which mode to use (train|test|profile|live)')
+                        help='which mode to use (train|test|profile|live|server)')
     parser.add_argument('--model', type=str, required=True,
                         help='which model to use (FRVSR|TecoGAN)')
     parser.add_argument('--opt', type=str, required=True,
@@ -376,6 +461,15 @@ if __name__ == '__main__':
         # run
         opt['is_train'] = False
         live(opt)
+
+    # ----------------- server ----------------- #
+    elif args.mode == 'server':
+        # setup paths
+        base_utils.setup_paths(opt, mode='test')
+
+        # run
+        opt['is_train'] = False
+        server(opt)
 
     # ----------------- profile ----------------- #
     elif args.mode == 'profile':
